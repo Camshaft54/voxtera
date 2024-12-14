6ef05bdec36145560943d9097da63fdb2eabfead
Camshaft54 <39676304+Camshaft54@users.noreply.github.com>
2024-12-14 15:43:32 -0500

    fix bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,8 +24,7 @@ class App extends Component {
             alert("Must choose a file to upload first!")
             return
         }
-        this.setState({status: 2})
-        this.setState({transcript: "Hi, I'm Brendan. We're here in the Fishworks Lab. Sorry for screaming. It's very loud in here with the air conditioners and servers all around me. We just made an interest in discovery and we thought we'd show you straight away. So over here, here I'm measuring disk IO operations broken down by latency. I've also drilled down to disk IO operations taking at least 520 milliseconds broken down by disk. This is using D-Trace so I can do performance analysis of disks. I'm applying a right workload to two JBugs which are over here. The lights are blinking on these JBugs because they're doing work. What I'm going to do is not recommend it. This is not supported. Do not try this at home. This is not special effects. This is real. What we're looking at here is the effect of disk vibration. Vibration is a serious issue and we can see the effect here that it's caused. So here the latency for the disks that are under the workload has gone up. Here I can see which disks have now had IO operations longer than 500 milliseconds and I can find out that they belong to the JBuds I was just shouting at. So amazing stuff. This has been made possible by analytics from fishworks which lets us look at things we've never seen before. High latency caused by vibration is a real issue. You've seen it. It does exist. Just don't let brethren shout at your disks. That's right. Don't shout at your JBugs. They don't like it."})
+        this.setState({status: 1})
         const {text} = await sendAudio(this.state.selectedFile)
         this.setState({transcript: text})
         this.setState({status: 2})
